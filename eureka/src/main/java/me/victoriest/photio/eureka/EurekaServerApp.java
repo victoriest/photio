b1e17bc423ecf5ce7c866a73e4af547b0711ff49
卫锴 <weik@wenduing.com>
2018-07-23 15:41:45 +0800

    init photio and create eureka server

---
+++ eureka/src/main/java/me/victoriest/photio/eureka/EurekaServerApp.java
@@ -0,0 +1,28 @@
+package me.victoriest.photio.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka服务注册中心
+ *
+ * 启动一个服务注册中心，只需要一个注解@EnableEurekaServer
+ * 配置文件参见application.yml
+ *
+ * profile文件为eureka-server
+ * mvn : clean spring-boot:run -P eureka-server
+ *
+ * @author VictoriEST
+ * @date 2017/12/11
+ * spring-cloud-step-by-step
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApp {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServerApp.class).web(true).run(args);
+    }
+
+}
