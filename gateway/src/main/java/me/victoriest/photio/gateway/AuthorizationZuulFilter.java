40ebacb0f6d16cafdea54d8f22bc8303c8e0e0cd
victoriest <heest0205@gmail.com>
2018-08-30 17:11:46 +0800

    init AuthorizationZuulFilter

---
+++ gateway/src/main/java/me/victoriest/photio/gateway/AuthorizationZuulFilter.java
@@ -0,0 +1,61 @@
+package me.victoriest.photio.gateway;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * zuul服务过滤
+ *
+ * @author VictoriEST
+ * @date 2018/1/26
+ * spring-cloud-step-by-step
+ */
+@Component
+public class AuthorizationZuulFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+//        filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+//        pre：路由之前
+//        routing：路由之时
+//        post： 路由之后
+//        error：发送错误调用
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        //filterOrder：过滤的顺序
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        // 这里可以写逻辑判断, 是否要过滤. true,永远过滤
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        // 滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        System.out.println(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            System.out.println("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        System.out.println("ok");
+        return null;
+    }
+}
